5091320f10624cb96748628758b7ecfd312510b0
Yeiska <yeiska83@yahoo.com>
2018-06-13 23:11:31 -0400

    game working 3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,79 +5,81 @@ var counter = 0;
 
 var resetAndStart = function () {
 
-	$(".crystals").empty();
+  $(".crystals").empty();
 
-	var images = [
-			'http://cdn.playbuzz.com/cdn/7a5d7935-6177-4be8-8b72-2a95ad2bcdfe/3b295cc9-7b5e-412f-8b1f-8547edd8e66b.jpg', 
-			'http://vignette3.wikia.nocookie.net/marvel-contestofchampions/images/1/1c/2-Star_Crystal.png/revision/latest?cb=20150825213642', 
-			'http://jonvilma.com/images/crystal-5.jpg', 
-			'https://static.turbosquid.com/Preview/2014/07/08__10_08_09/Crystals0010.jpgc22b2831-ae7a-4cb6-b4ac-612aa7f35ad7Original.jpg'];
-		
-	random_result = Math.floor(Math.random() * 101 ) + 19; 
+  var images = [
+    'http://cdn.playbuzz.com/cdn/7a5d7935-6177-4be8-8b72-2a95ad2bcdfe/3b295cc9-7b5e-412f-8b1f-8547edd8e66b.jpg',
+    'http://vignette3.wikia.nocookie.net/marvel-contestofchampions/images/1/1c/2-Star_Crystal.png/revision/latest?cb=20150825213642',
+    'http://jonvilma.com/images/crystal-5.jpg',
+    'https://static.turbosquid.com/Preview/2014/07/08__10_08_09/Crystals0010.jpgc22b2831-ae7a-4cb6-b4ac-612aa7f35ad7Original.jpg'
+  ];
 
-
-	$("#result").html('Random Result: ' + random_result);
-
-	for(var i = 0; i < 4; i++){
-
-		var random = Math.floor(Math.random() * 11) + 1;
-
-		var crystal = $("<div>");
-			crystal.attr({
-				"class": 'crystal',
-				"data-random": random
-			});
-			crystal.css({
-				"background-image":"url('" + images[i] + "')",
-				"background-size":"cover"
-
-			});
+  random_result = Math.floor(Math.random() * 101) + 19;
 
 
-		$(".crystals").append(crystal);
+  $("#result").html('Random Result: ' + random_result);
 
-	}
+  for (var i = 0; i < 4; i++) {
 
-	$("#counter").html("Total Score: " + counter);
+    var random = Math.floor(Math.random() * 11) + 1;
+
+    var crystal = $("<div>");
+    crystal.attr({
+      "class": 'crystal',
+      "data-random": random
+    });
+    crystal.css({
+      "background-image": "url('" + images[i] + "')",
+      "background-size": "cover"
+
+    });
+
+
+    $(".crystals").append(crystal);
+
+  }
+
+  $("#counter").html("Total Score: " + counter);
 
 }
 
 
 resetAndStart();
 
+
+
 $(document).on('click', ".crystal", function () {
 
-	var num = parseInt($(this).attr('data-random'));
+  var num = parseInt($(this).attr('data-random'));
 
-	counter += num;
+  counter += num;
 
 
-	$("#counter").html("Total score: " + counter);
+  $("#counter").html("Total score: " + counter);
 
-	console.log(counter);
+  console.log(counter);
 
-	if(counter > random_result){
+  if (counter > random_result) {
 
-		lost++;
+    lost++;
 
-		$("#lost").html("You lost: " + lost);
+    $("#lost").html("You lost: " + lost);
 
-		counter = 0;
+    counter = 0;
 
-		resetAndStart();
+    resetAndStart();
 
-	} 
-	else if(counter === random_result)
+  }
+  else if (counter === random_result) {
 
-		win++;
+    win++;
 
-		$("#win").html("You win: " + win);
+    $("#win").html("You win: " + win);
 
-		counter = 0;
+    counter = 0;
 
-		resetAndStart();
-
-	}
+    resetAndStart();
+  }
 
 });
 
